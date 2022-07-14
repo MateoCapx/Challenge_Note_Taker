58150aae249b52ae54780bcb6b20eec87b40fc4c
Mateo <mateom.realtor@gmail.com>
2022-07-14 15:23:37 -0400

    updated files

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,11 +1,16 @@
 const { Router} = require('express');
-// const fs = require('fs');
-const store = require('store');
+ const fs = require('fs');
+// const store = require('store');
 
 const router = Router();
 
 router.get("/api/notes", (req, res) => {
-  return  store.getNotes()
+  //'fs' to read file from db folder where users data is saved
+    fs.readFile("/db.json", "utf-8").then(function(data) {
+        notes =[].concat(JSON.parse(data))
+        res.json(notes);
+    })
+//   return  store.getNotes()
   });
 
 
@@ -13,11 +18,3 @@ module.exports = router;
 
 
 
-
-
-//   //'fs' to read file from db folder where users data is saved
-//     fs.readFile("/Users/Mateo/Desktop/GitHub/Challenge_Note_Taker/db.json", "utf-8", (err, data) => {
-//       if (err) throw err;
-//       res.json(JSON.parse(data));
-     
-//     });
