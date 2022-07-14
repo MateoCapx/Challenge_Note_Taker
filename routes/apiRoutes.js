d4639b033725bbba2417ade309ed10555e9aa18e
Mateo <mateom.realtor@gmail.com>
2022-07-14 15:37:10 -0400

    updated

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,15 +1,19 @@
 const { Router} = require('express');
  const fs = require('fs');
+ const notes =require('../db.json')
+console.log('these are the notes: ', notes)
 // const store = require('store');
 
 const router = Router();
 
-router.get("/api/notes", (req, res) => {
+router.get("/notes", (req, res) => {
   //'fs' to read file from db folder where users data is saved
-    fs.readFile("/db.json", "utf-8").then(function(data) {
-        notes =[].concat(JSON.parse(data))
-        res.json(notes);
-    })
+    // fs.readFile("../db.json", "utf-8" , function(err, data) {
+    //     // notes =[].concat(JSON.parse(data))
+    //     console.log(data)
+       
+    // })
+     res.json(notes);
 //   return  store.getNotes()
   });
 
