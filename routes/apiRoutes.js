2b7f20d8756e7a9156ec663c2180837dc9b028dd
Mateo <mateom.realtor@gmail.com>
2022-07-14 13:41:42 -0400

    update

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,9 +1,15 @@
-const router = require('express').Router();
+const { Router} = require('express');
+const fs = require('fs');
 
-router.get('/db.json' ,(request,response)=>{
-    console.log(request.body)
+const router = Router();
 
-
-})
-
+router.get("/api/notes", (req, res) => {
+    //'fs' to read file from db folder where users data is saved
+    fs.readFile("/Users/Mateo/Desktop/GitHub/Challenge_Note_Taker/db.json", "utf-8", (err, data) => {
+      if (err) throw err;
+      res.json(JSON.parse(data));
+     
+    });
+  });
 module.exports = router;
+
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const apiRoutes = require('./routes/apiRoutes');
 const htmlRoutes = require('./routes/htmlRoutes');
 
+
 // Initialize the app and create a port
 const app = express();
 const PORT = process.env.PORT || 3001;
