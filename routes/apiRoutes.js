e8293c740ee57f57fba23573381247d0326034f5
Mateo <mateom.realtor@gmail.com>
2022-07-18 16:05:39 -0400

    Updated the writefile

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,45 +1,52 @@
-const { Router, response} = require('express');
- const fs = require('fs');
- const notes = require('../db.json')
- const path = require('path')
-// console.log('these are the notes: ', notes)
+const { Router, response } = require('express');
+const fs = require('fs');
+const notes = require('../db.json')
+const path = require('path')
 
 
 const router = Router();
 
 router.get("/notes", (req, res) => {
-  //'fs' to read file from db folder where users data is saved
-    fs.readFile("db.json", "utf8" , function(err, data) {
-          data = [].concat(JSON.parse(data))
-        
+    //'fs' to read file from db folder where users data is saved
+    fs.readFile("db.json", "utf8", function (err, data) {
+        data = [].concat(JSON.parse(data))
+
     })
-    // res.sendFile(path.join(__dirname, 'db.json'));
-  
+
     console.log(notes)
-     return res.json(notes);
+    return res.json(notes);
 
-  });
+});
 
 
-  let testObject =
-  [
-    {
-        "id": 2,
-        "title":"Learn SQL",
-        "text":"SQL Rocks"
-    }
-]
+// let testObject =
+// {
+//     "id": 2,
+//     "title": "Learn SQL",
+//     "text": "SQL Rocks"
+// }
 
-  router.post('/notes',(request,response)=>{
+
+router.post('/notes', (request, response) => {
     console.log(request.body);
-    fs.writeFile("db.json", JSON.stringify.push(testObject) , function(err, data) {
-        // data = [].concat(JSON.stringify(data))
-      if(err){
-          console.log(`Error: ${err}`)
-      }
-  })
+    fs.readFile("db.json", "utf8", function (err, data) {
+        data = [].concat(JSON.parse(data))
+        console.log("My Log:", data)
+        // data.push(data)
+
+        fs.writeFile("db.json", JSON.stringify(request), function (err, data) {
+            //  data = [].concat(JSON.stringify(data))
+            if (err) {
+                console.log(`Error: ${err}`)
+            }
+           
+        })
+
+    })
+
+
     response.send(201)
-  });
+});
 
 
 module.exports = router;
